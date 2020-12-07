e420e0704fdee21628b0084faf79be736d1e03b9
Iryna <ortofosfatnyi@gmail.com>
2020-12-07 16:48:23 +0200

    Added BusinessException

---
+++ BusinessException.cs
@@ -0,0 +1,19 @@
+﻿namespace Homework11
+{
+    using System;
+
+    /// <summary>
+    /// Added class from BusinessException.
+    /// </summary>
+    public class BusinessException : Exception
+    {
+        /// <summary>
+        /// Make metod BusinessException from our Exception.
+        /// </summary>
+        /// <param name="message"></param>
+        public BusinessException(string message)
+           : base(message)
+        {
+        }
+    }
+}
