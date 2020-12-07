a4fb8fe9f510c89480e0b164ce3f209fe102d2fa
Iryna <ortofosfatnyi@gmail.com>
2020-12-07 18:23:44 +0200

    Added a text entry (not yet possible to delete)

--- FileService.cs
+++ FileService.cs
@@ -1,6 +1,8 @@
 ﻿namespace Homework11
 {
     using System;
+    using System.IO;
+    using System.Reflection;
 
     /// <summary>
     /// FileService for writing the logger to a file.
@@ -8,32 +10,17 @@
     public class FileService
     {
         /// <summary>
-        /// Logic of writing to a file.
+        /// A method that describes the saving of text in txt.
         /// </summary>
-        public void FileLogic()
+        /// <param name="typeLog"> Responsible for entering the log type.</param>
+        /// <param name="message"> Responsible for entering the message log.</param>
+        public static void WriteLine(string typeLog, string message)
         {
-            // Logger logger = Logger.Instance;
-
-            // string books = @"E:\log.txt";
-
-            // using (StreamWriter sw = File.AppendText(books))
-            // sw.Write(logger.LogMessage());
-
-            // using (FileStream sw = File.Open(books, FileMode.Open))
-
-            // try
-            // {
-            //    StreamReader sr = new StreamReader(@"E:\log.txt");
-            //    while (!sr.EndOfStream)
-            //    {
-            //        Console.WriteLine(sr.ReadLine());
-            //    }
-            //    sr.Close();
-            // }
-            // catch (FileNotFoundException)
-            // {
-            //    Console.WriteLine("File does not exist!");
-            // }
+            // создание тхт файла
+            using (StreamWriter sw = new StreamWriter(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "Log.txt", true))
+            {
+                sw.WriteLine(string.Format("{0}: {1}: {2}", DateTime.Now.ToLongTimeString(), typeLog, message));
+            }
         }
     }
 }
