0ea4c943285b45394aef831a135f11ba93f409db
Iryna <ortofosfatnyi@gmail.com>
2020-12-07 18:57:53 +0200

    Added new Directory and file Log.txt inside this folder

--- BusinessException.cs
+++ BusinessException.cs
@@ -8,6 +8,7 @@
     public class BusinessException : Exception
     {
         /// <summary>
+        /// Initializes a new instance of the <see cref="BusinessException"/> class.
         /// Make metod BusinessException from our Exception.
         /// </summary>
         /// <param name="message"> From message about Error.</param>
--- FileService.cs
+++ FileService.cs
@@ -16,8 +16,17 @@
         /// <param name="message"> Responsible for entering the message log.</param>
         public static void WriteLine(string typeLog, string message)
         {
+            string dirpath = @"E:\Log";
+            DirectoryInfo dirInfo = new DirectoryInfo(dirpath);
+            if (!dirInfo.Exists)
+            {
+                dirInfo.Create();
+            }
+
+            string writePath = @"E:\Log\Log.txt";
+
             // создание тхт файла
-            using (StreamWriter sw = new StreamWriter(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "Log.txt", true))
+            using (StreamWriter sw = new StreamWriter(writePath, true, System.Text.Encoding.Default))
             {
                 sw.WriteLine(string.Format("{0}: {1}: {2}", DateTime.Now.ToLongTimeString(), typeLog, message));
             }
