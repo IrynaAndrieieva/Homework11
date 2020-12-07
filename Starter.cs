98ba55cb59e12d3d42dd59f52cce99e323c7a663
Iryna <ortofosfatnyi@gmail.com>
2020-12-07 16:53:15 +0200

    Added Run with loop for errors

--- Starter.cs
+++ Starter.cs
@@ -5,10 +5,35 @@
     /// <summary>
     /// Start buisness logic.
     /// </summary>
-    class Starter
+    public class Starter
     {
+        /// <summary>
+        /// Maked finaly logic.
+        /// </summary>
         public void Run()
         {
+            Actions actions = new Actions();
+
+            Random rnd = new Random();
+            int value = rnd.Next(0, 100);
+
+            for (int numbers = 0; numbers < 100; numbers++)
+            {
+                if ((numbers + value) % 2 == 0)
+                {
+                    actions.LoggerInfo();
+                }
+                else if ((numbers + value) % 3 == 0)
+                {
+                    actions.LoggeWarning();
+                }
+                else
+                {
+                    actions.LoggerError();
+                }
+            }
+
+            Console.ReadKey();
         }
     }
 }
