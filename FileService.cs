443540493e34fa18aa170148db8873141183f0a0
Iryna <ortofosfatnyi@gmail.com>
2020-12-07 17:04:33 +0200

    Attempt make commit from StyleCop in FileService

---
+++ FileService.cs
@@ -0,0 +1,39 @@
+﻿namespace Homework11
+{
+    using System;
+
+    /// <summary>
+    /// FileService for writing the logger to a file.
+    /// </summary>
+    public class FileService
+    {
+        /// <summary>
+        /// Logic of writing to a file.
+        /// </summary>
+        public void FileLogic()
+        {
+            // Logger logger = Logger.Instance;
+
+            // string books = @"E:\log.txt";
+
+            // using (StreamWriter sw = File.AppendText(books))
+            // sw.Write(logger.LogMessage());
+
+            // using (FileStream sw = File.Open(books, FileMode.Open))
+
+            // try
+            // {
+            //    StreamReader sr = new StreamReader(@"E:\log.txt");
+            //    while (!sr.EndOfStream)
+            //    {
+            //        Console.WriteLine(sr.ReadLine());
+            //    }
+            //    sr.Close();
+            // }
+            // catch (FileNotFoundException)
+            // {
+            //    Console.WriteLine("File does not exist!");
+            // }
+        }
+    }
+}
