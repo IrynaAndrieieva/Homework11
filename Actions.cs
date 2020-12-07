21f6631a5ac72c1481e10a2b52c5c1336b168725
Iryna <ortofosfatnyi@gmail.com>
2020-12-07 16:45:36 +0200

    Attempt make commit from StyleCop in Actions

--- Actions.cs
+++ Actions.cs
@@ -12,11 +12,17 @@
         /// </summary>
         Logger logger = Logger.Instance;
 
+        /// <summary>
+        /// Make message from Info.
+        /// </summary>
         public void LoggerInfo()
         {
             this.logger.LogMessage("Info", "Start method");
         }
 
+        /// <summary>
+        /// Maked message from Warning and attempt make BusinessException.
+        /// </summary>
         public void LoggeWarning()
         {
             this.logger.LogMessage("Warning", "Action got this custom Exception"); 
@@ -24,6 +30,9 @@
             //throw new BusinessException("Skipped logic in method: BusinessException");
         }
 
+        /// <summary>
+        /// Maked message from Error and maked Exception.
+        /// </summary>
         public void LoggerError()
         {
             this.logger.LogMessage("Error", "Action failed by reason");
